a55a6c965380eb3296e06869de087e3ca4ea4d4b
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-01-02 07:24:53 -0500

    initial attempt at using bitcoin-api to check balance on a wallet
    this isn't yet working quite right..

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
 module Main where
 
+import  Data.Base58String.Bitcoin   (fromText)
+import  Data.Bitcoin.Types          (Address)
+import  Data.Text                   (pack)
+import  Network.Bitcoin.Api.Client  (Client, withClient)
+import  Network.Bitcoin.Api.Misc    (getInfo)
+import  Network.Bitcoin.Api.Wallet  (getAccountBalance, getAddressAccount)
+
+localClient :: (Client -> IO a) -> IO a
+localClient = withClient "127.0.0.1" 18332 (pack "username") (pack "password")
+
+addr :: Address
+addr = fromText $ pack "wallet_address"
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  localClient $ \client -> do
+    account <- getAddressAccount client addr
+    balance <- getAccountBalance client account
+    info    <- getInfo client
+
+    putStrLn $ "info: "       ++ show info
+    putStrLn $ "account: "    ++ show account
+    putStrLn $ "balance: "    ++ show balance
